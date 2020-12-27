d07757175b828db449fb615ae68f508a180a08c4
Hahn Jiho <ferrarijm@naver.com>
2020-12-27 18:03:01 +0900

    fix: For PR

--- app/src/main/java/com/trace/myapplication/LoginActivity.kt
+++ app/src/main/java/com/trace/myapplication/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.trace.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +10,12 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
 
-        btn_login.setOnClickListener(){
-            startActivity(Intent(this, MainActivity::class.java));
-            finish()
-        }
-        btn_to_signup.setOnClickListener {
-            startActivity(Intent(this, SignupActivity::class.java))
-        }
+//        btn_login.setOnClickListener(){
+//            startActivity(Intent(this, MainActivity::class.java));
+//            finish()
+//        }
+//        btn_to_signup.setOnClickListener {
+//            startActivity(Intent(this, SignupActivity::class.java))
+//        }
     }
 }
