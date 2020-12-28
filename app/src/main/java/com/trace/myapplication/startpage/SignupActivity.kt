6e3d9f607965d2b388f87d7eacf86f8cbc685f04
Hahn Jiho <ferrarijm@naver.com>
2020-12-28 20:28:10 +0900

    fix: For PR #2

--- app/src/main/java/com/trace/myapplication/startpage/SignupActivity.kt
+++ app/src/main/java/com/trace/myapplication/startpage/SignupActivity.kt
@@ -2,11 +2,31 @@ package com.trace.myapplication.startpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.trace.myapplication.R
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var appBarSignupConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val navHostSignupFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_fragment_signup) as NavHostFragment
+        //val navSignupController = navHostSignupFragment.navController
+        //appBarSignupConfiguration = AppBarConfiguration(navSignupController.graph)
+        //setupActionBarWithNavController(navSignupController, appBarSignupConfiguration)
+        //setupActionBarWithNavController(navSignupController)
     }
+/* Appbar setting
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_signup)
+        return navController.navigateUp(appBarSignupConfiguration)
+                || super.onSupportNavigateUp()
+    }
+ */
 }
